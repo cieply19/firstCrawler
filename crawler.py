83b60fa303e7537f6337e0e65d9dee5ad1dcfc79
cieply19 <31536474+cieply19@users.noreply.github.com>
2021-10-19 10:05:50 +0200

    Create crawler.py

---
+++ crawler.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+import csv, datetime
+
+response = requests.get("https://e-kursy-walut.pl/")
+soup = BeautifulSoup(response.text, 'html.parser')
+
+crypto = ('Bitcoin', 'Ethereum', 'Cardano', 'Binance Coin', 'Solana', 'BitTorrent', 'Ripple', 'Litecoin', 'Iota')
+
+rows = []
+for x in crypto:
+    usd_rate = soup.find(text=x).next_element.small.text
+    pln_rate = soup.find(text=x).next_element.strong.text
+
+    rows.append([x, usd_rate, pln_rate, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")])
+
+# zapis do pliku csv
+
+filename = r'c:\temp\kursy.csv'
+fields = ['Nazwa waluty', 'Kurs PLN', 'Kurs USD', 'Data + Time']
+
+with open(filename, 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(fields)
+    csvwriter.writerows(rows)
